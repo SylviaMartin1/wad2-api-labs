70b708ce7c74b9e949869a3e1eb1ffb244defd09
sylvi <20102981@mail.wit.ie>
2025-11-25 12:52:53 +0000

    Added Auth Middleware

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -4,6 +4,8 @@ import tasksRouter from './api/tasks';
 import './db';
 import cors from 'cors';
 import usersRouter from './api/users';
+import authenticate from './authenticate';
+
 
 dotenv.config();
 
@@ -23,9 +25,9 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 app.use('/api/users', usersRouter);
+
 
 app.use(errHandler);
 
